c9fad4102669944d50be200426aab330fb0ca3ac
Martin Wam <martin.wam@gmail.com>
2022-02-18 13:18:37 +0100

    MOVE-2733: fixed test

--- src/test/java/no/digdir/efmesindexreader/service/ElasticsearchWebClientTest.java
+++ src/test/java/no/digdir/efmesindexreader/service/ElasticsearchWebClientTest.java
@@ -1,85 +1,41 @@
 package no.digdir.efmesindexreader.service;
 
-import no.digdir.efmesindexreader.EfmEsIndexReaderApplication;
-import no.digdir.efmesindexreader.domain.data.EsIndexDTO;
 import no.digdir.efmesindexreader.handler.EsIndexHandler;
 import no.digdir.efmesindexreader.router.EsIndexRouter;
-import okhttp3.mockwebserver.MockWebServer;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.IOException;
-
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureWebTestClient
-//@ExtendWith(SpringExtension.class)
-//@WebMvcTest(EsIndexRouter.class)
-//@AutoConfigureWebFlux
-@ContextConfiguration(classes = EfmEsIndexReaderApplication.class)
-@WebFluxTest(controllers = EsIndexRouter.class,excludeAutoConfiguration = {ReactiveSecurityAutoConfiguration.class})
-@TestPropertySource("classpath:/config/application-test.properties")
-@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(excludeAutoConfiguration = {ReactiveSecurityAutoConfiguration.class})
+@ContextConfiguration(classes = {EsIndexRouter.class, EsIndexHandler.class})
 public class ElasticsearchWebClientTest {
-
-    private static MockWebServer mockWebServer;
-
-//    @Autowired
-//    private EsIndexReaderProperties properties;
-
 
     @Autowired
     private WebTestClient webTestClient;
-
-    @MockBean(name = "EsWebClient")
-    WebClient webClient;
-    @MockBean
-    private EsIndexHandler handler;
 
     @MockBean
     private LoggingProxySender loggingProxySender;
 
     @MockBean
-    private WebClientConfiguration webClientConfiguration;
+    private ElasticsearchIngestService elasticsearchIngestService;
 
-    @BeforeAll
-    public static void setUp() throws IOException {
-        mockWebServer = new MockWebServer();
-        mockWebServer.start();
-    }
-
-    @BeforeEach
-    public void initialize() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @AfterAll
-    public static void tearDown() throws IOException {
-        mockWebServer.shutdown();
-
-    }
     @Test
     public void openScrollIndex_shouldOpenScrollSuccessfully() {
-        //webTestClient.get().uri("/esindex/?index=graylog_0")
         webTestClient.get().uri("/esindex/all")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(EsIndexDTO.class)
-                .consumeWith(response -> Assertions.assertThat(response.getResponseBody()).isNotNull());
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .consumeWith(r -> Assertions.assertEquals("i'm a placeholder for a list of collected indexes which requires a DB", new String(r.getResponseBody())));
 
     }
 }
